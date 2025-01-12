abd6e72b2f92710efb5870945ed1951b63d12848
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-12 19:18:48 +0530

    feat: add next-themes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import BreadcrumHeader from "@/components/BreadcrumHeader";
 import DesktopSidebar from "@/components/DesktopSidebar";
+import { ModeToggle } from "@/components/ThemeModeToggle";
 import { Separator } from "@/components/ui/separator";
 import React from "react";
 
@@ -10,10 +11,13 @@ type Props = {
 const layout = ({ children }: Props) => {
   return (
     <div className="flex h-screen">
-      <DesktopSidebar/>
+      <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
-          <BreadcrumHeader/>
+          <BreadcrumHeader />
+          <div className="gap-1 flex items-center">
+            <ModeToggle />
+          </div>
         </header>
         <Separator />
         <div className="overflow-auto">
