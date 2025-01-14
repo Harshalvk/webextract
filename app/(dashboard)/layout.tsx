1e98898ab1116c0375d14cc2b2f44fc14d2cd7e0
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-14 22:04:02 +0530

    add(ui): user avatar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,21 +1,35 @@
+import { auth } from "@/auth";
 import BreadcrumHeader from "@/components/BreadcrumHeader";
 import DesktopSidebar from "@/components/Sidebar";
 import { ModeToggle } from "@/components/ThemeModeToggle";
 import { Separator } from "@/components/ui/separator";
+import UserAccount from "@/components/UserAccount";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const layout = ({ children }: Props) => {
+const layout = async ({ children }: Props) => {
+  const session = await auth();
+  const user = session?.user;
+  if (!user) {
+    redirect("/signup");
+  }
   return (
     <div className="flex h-screen">
       <DesktopSidebar />
       <div className="flex flex-col flex-1 min-h-screen">
         <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
           <BreadcrumHeader />
-          <div className="gap-1 flex items-center">
+          <div className="gap-2 flex items-center">
+            {user ? (
+              <UserAccount user={user} />
+            ) : (
+              <Link href={"/signup"}>Signup</Link>
+            )}
             <ModeToggle />
           </div>
         </header>
---
+++ components/UserAccount.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { User } from "next-auth";
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import UserAvatar from "./UserAvatar";
+import { LogOut } from "lucide-react";
+
+type Props = {
+  user: Pick<User, "name" | "image" | "email">;
+};
+
+const UserAccount = ({ user }: Props) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar user={user} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white dark:bg-zinc-900" align="end">
+        <div className="flex items-center justify-star gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-zinc-700 dark:text-zinc-400">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href={"/"}>Home</Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            signOut();
+          }}
+          className="text-red-800 cursor-pointer dark:text-red-500 group"
+        >
+          Sign Out{" "}
+          <LogOut
+            height={16}
+            width={16}
+            className="ml-1 group-hover:translate-x-1 transition-all"
+          />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserAccount;
