67ca46422ed9de2e9bdd459f7e54b7db9ce0bae6
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-15 11:21:52 +0530

    feat(state-management): Add & setup tanstack-query for state management

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  
   return (
-    <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
