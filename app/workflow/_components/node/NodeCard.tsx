9c2c22bf40c620c8907558fc64b746e77210ade5
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-25 23:39:52 +0530

    feat: add node zoom when double clicked

--- app/workflow/_components/node/NodeCard.tsx
+++ app/workflow/_components/node/NodeCard.tsx
@@ -1,14 +1,42 @@
-"use client"
+"use client";
 
+import { cn } from "@/lib/utils";
+import { useReactFlow } from "@xyflow/react";
 import React from "react";
 
 type Props = {
   nodeId: string;
   children: React.ReactNode;
+  isSelected: boolean;
 };
 
-const NodeCard = ({ nodeId, children }: Props) => {
-  return <div>{children}</div>;
+const NodeCard = ({ nodeId, children, isSelected }: Props) => {
+  const { getNode, setCenter } = useReactFlow();
+
+  return (
+    <div
+      onDoubleClick={() => {
+        const node = getNode(nodeId);
+        if (!node) return;
+        const { position, measured } = node;
+        if (!position || !measured) return;
+        const { width, height } = measured;
+        const x = position.x + width! / 2;
+        const y = position.y + height! / 2;
+        if (x === undefined || y === undefined) return;
+        setCenter(x, y, {
+          zoom: 1,
+          duration: 500,
+        });
+      }}
+      className={cn(
+        "w-[420px] border-2 border-separate bg-background p-2 rounded-md cursor-pointer text-xs gap-1 flex flex-col",
+        isSelected && "border-primary"
+      )}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default NodeCard;
