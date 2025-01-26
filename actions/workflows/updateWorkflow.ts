31a4805e0d261dc990ecbfe907396ab9ca6a67cc
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-26 15:50:25 +0530

    add: workflow action to udpate workflow

---
+++ actions/workflows/updateWorkflow.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import { WorkflowStatus } from "@/types/workflow.types";
+
+export async function UpdateWorkflow({
+  id,
+  definition,
+}: {
+  id: string;
+  definition: string;
+}) {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    throw new Error("Unauthenticated");
+  }
+
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+
+  if (!workflow) throw new Error("Workflow not found");
+  if (workflow.status !== WorkflowStatus.DRAFT) {
+    throw new Error("Workflow is not a drat");
+  }
+
+  await prisma.workflow.update({
+    data: {
+      definition,
+    },
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+}
