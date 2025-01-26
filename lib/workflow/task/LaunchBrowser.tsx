ba4f4534449debe7bf782af4d15e122930e9ea5c
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-26 14:49:42 +0530

    add: extend object with inputs array

--- lib/workflow/task/LaunchBrowser.tsx
+++ lib/workflow/task/LaunchBrowser.tsx
@@ -1,11 +1,18 @@
-import { TaskType } from "@/types/task.types";
+import { TaskParamType, TaskType } from "@/types/task.types";
 import { Globe, LucideProps } from "lucide-react";
 
 export const LaunchBrowserTask = {
   type: TaskType.LAUNCH_BROWSER,
   label: "Launch browser",
-  icon: (props: LucideProps) => (
-    <Globe className="stroke-primary" {...props} />
-  ),
+  icon: (props: LucideProps) => <Globe className="stroke-primary" {...props} />,
   isEntryPoint: true,
+  inputs: [
+    {
+      name: "Website Url",
+      type: TaskParamType.STRING,
+      helperText: "eg: https://www.github.com",
+      required: true,
+      hideHandle: true,
+    },
+  ],
 };
