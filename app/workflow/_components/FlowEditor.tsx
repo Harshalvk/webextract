25d759df148e2d9c2b95cf8efcca9b73325ef84a
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-27 23:09:24 +0530

    feat: get node definition from db>worflow and render on the canvas

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { Workflow } from "@prisma/client";
 import {
   Background,
-  BackgroundVariant,
   Controls,
   ReactFlow,
   useEdgesState,
   useNodesState,
+  useReactFlow,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { CreateFlowNode } from "@/lib/workflow/createFlowNode";
-import { TaskType } from "@/types/task.types";
 import NodeComponent from "./node/NodeComponent";
 
 type Props = {
@@ -28,10 +26,24 @@ const snapGrid: [number, number] = [50, 50];
 const fitViewOptions = { padding: 2 };
 
 const FlowEditor = ({ workflow }: Props) => {
-  const [nodes, setNodes, onNodesChange] = useNodesState([
-    CreateFlowNode(TaskType.LAUNCH_BROWSER),
-  ]);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { setViewport } = useReactFlow();
+
+  useEffect(() => {
+    try {
+      const flow = JSON.parse(workflow.definition);
+      if (!flow) return;
+      setNodes(flow.nodes || []);
+      setEdges(flow.edges || []);
+
+      if (flow.viewport) return;
+      const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+      setViewport({ x, y, zoom });
+    } catch (error) {
+      
+    }
+  }, [workflow.definition, setEdges, setNodes, setViewport]);
 
   return (
     <main className="h-full w-full">
