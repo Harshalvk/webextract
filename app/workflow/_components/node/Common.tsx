16a3c4d5fb071419bcacf50102c646c30ffcb803
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-29 23:17:54 +0530

    add: config for node handle colors

---
+++ app/workflow/_components/node/Common.tsx
@@ -0,0 +1,6 @@
+import { TaskParamType } from "@/types/task.types";
+
+export const ColorForHandle: Record<TaskParamType, string> = {
+  BROWSER_INSTANCE: "!bg-sky-400",
+  STRING: "!bg-amber-400",
+};
