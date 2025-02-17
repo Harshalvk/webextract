82ddc384070e54cd55ce68d9290d463fce1cb9fe
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-17 22:03:38 +0530

    fix: add custom component render logic

--- app/workflow/_components/node/param/StringParam.tsx
+++ app/workflow/_components/node/param/StringParam.tsx
@@ -1,11 +1,26 @@
+import { AutosizeTextarea } from "@/components/ui/auto-resize-textarea";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ParamProps } from "@/types/appNode.types";
-import React, { useId, useState } from "react";
+import React, { useEffect, useId, useState } from "react";
 
-const StringParam = ({ param, value, udpateNodeParamValue }: ParamProps) => {
+const StringParam = ({
+  param,
+  value,
+  udpateNodeParamValue,
+  disabled,
+}: ParamProps) => {
   const [internalValue, setInternalValue] = useState("");
   const id = useId();
+
+  useEffect(() => {
+    setInternalValue(value);
+  }, [value]);
+
+  let Component: any = Input;
+  if (param.variant === "textarea") {
+    Component = AutosizeTextarea;
+  }
 
   return (
     <div className="space-y-1 p-1 w-full">
@@ -13,13 +28,14 @@ const StringParam = ({ param, value, udpateNodeParamValue }: ParamProps) => {
         <p className="text-muted-foreground font-semibold">{param.name}</p>
         {param.required && <p className="text-red-400 px-2">*</p>}
       </Label>
-      <Input
+      <Component
         id={id}
+        disabled={disabled}
         className="text-xs"
         value={internalValue}
         placeholder="Enter value here"
-        onChange={(e) => setInternalValue(e.target.value)}
-        onBlur={(e) => udpateNodeParamValue(e.target.value)}
+        onChange={(e: any) => setInternalValue(e.target.value)}
+        onBlur={(e: any) => udpateNodeParamValue(e.target.value)}
       />
       {param.helperText && (
         <p className="text-muted-foreground px-2">{param.helperText}</p>
