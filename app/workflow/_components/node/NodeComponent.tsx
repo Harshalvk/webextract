daf1caa315263941f7d3ab0a38878ebfc1ea0fd8
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-19 22:53:56 +0530

    feat: add node ID badge in development mode

--- app/workflow/_components/node/NodeComponent.tsx
+++ app/workflow/_components/node/NodeComponent.tsx
@@ -6,13 +6,16 @@ import { IAppNodeData } from "@/types/appNode.types";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { NodeInput, NodeInputs } from "./NodeInputs";
 import { NodeOutput, NodeOutputs } from "./NodeOutputs";
+import { Badge } from "@/components/ui/badge";
 
+const DEV_MODE = process.env.NEXT_PUBLIC_DEV_MODE === "true"
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as IAppNodeData;
   const task = TaskRegistry[nodeData.type];
 
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
+      {DEV_MODE && <Badge>{props.id}</Badge>}
       <NodeHeader taskType={nodeData.type} nodeId={props.id} />
       <NodeInputs>
         {task.inputs.map((input) => (
