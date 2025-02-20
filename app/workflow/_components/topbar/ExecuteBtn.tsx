510bf76b562de2fc13b8473eb53622d5e65ca702
Harshalvk <harshalvkhobragade@gmail.com>
2025-02-20 13:34:18 +0530

    feat: implement workflow execution with server action and mutation

--- app/workflow/_components/topbar/ExecuteBtn.tsx
+++ app/workflow/_components/topbar/ExecuteBtn.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { RunWorkflow } from "@/actions/workflows/runWorkflow";
 import useExecutionPlan from "@/components/hooks/useExecutionPlan";
 import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { useReactFlow } from "@xyflow/react";
 import { Play } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 type Props = {
   workflowId: string;
@@ -11,13 +15,34 @@ type Props = {
 
 const ExecuteBtn = ({ workflowId }: Props) => {
   const generate = useExecutionPlan();
+  const { toObject } = useReactFlow();
+
+  const mutation = useMutation({
+    mutationFn: RunWorkflow,
+    onSuccess: () => {
+      toast.success("Execution started", { id: "flow-execution" });
+    },
+    onError: () => {
+      toast.error("Something went wrong", { id: "flow-execution" });
+    },
+  });
 
   return (
     <Button
       variant={"outline"}
       className="flex items-center gap-2"
+      disabled={mutation.isPending}
       onClick={() => {
         const plan = generate();
+        if (!plan) {
+          // client side validation
+          return;
+        }
+
+        mutation.mutate({
+          workflowId: workflowId,
+          flowDefinition: JSON.stringify(toObject()),
+        });
       }}
     >
       <Play size={116} className="stroke-orange-400" /> Execute
