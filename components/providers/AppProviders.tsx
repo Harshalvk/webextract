9f7963e9160a3eca2e535e071876d8ae626af45e
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-06 23:19:46 +0530

    feat: add NextTopLoader for page navigation progress indicator

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -4,16 +4,18 @@ import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-  
+
   return (
     <SessionProvider>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
-        {children}
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <NextTopLoader color={"#a1a1aa"} showSpinner={false} />
+        <ThemeProvider attribute={"class"} defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
       </QueryClientProvider>
     </SessionProvider>
   );
