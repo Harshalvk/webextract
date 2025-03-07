e305341a4efab0fe66a01925baa65b8fe98b24dc
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-08 02:15:04 +0530

    feat: add server action to update workflow cron schedule

---
+++ actions/workflows/updateWorkflowCron.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import parser from "cron-parser";
+import { revalidatePath } from "next/cache";
+
+export async function UpdateWorkflowCron({
+  id,
+  cron,
+}: {
+  id: string;
+  cron: string;
+}) {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    throw new Error("Unauthenticated");
+  }
+
+  try {
+    const interval = parser.parse(cron, { tz: "utc" });
+    await prisma.workflow.update({
+      where: { id, userId: user.id },
+      data: {
+        cron,
+        nextRunAt: interval.next().toDate(),
+      },
+    });
+  } catch (error: any) {
+    console.error("invalid cron:", error.message);
+    throw new Error("invalid cron expression");
+  }
+
+  revalidatePath("/workflows");
+}
