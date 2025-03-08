297ea02270e50b513b14a3da00dfe53c8017a123
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-08 14:12:57 +0530

    feat: fix typo in indicatorColors and add ExecutionStatusLabel component

--- app/workflow/runs/[workflowId]/_components/ExecutionStatusIndicator.tsx
+++ app/workflow/runs/[workflowId]/_components/ExecutionStatusIndicator.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import { WorkflowExecutionStatus } from "@/types/workflow.types";
 import React from "react";
 
-const indicatroColors: Record<WorkflowExecutionStatus, string> = {
+const indicatorColors: Record<WorkflowExecutionStatus, string> = {
   PENDING: "bg-slate-400 animate-pulse",
   RUNNING: "bg-yellow-400 animate-pulse",
   FAILED: "bg-red-400",
@@ -15,6 +15,21 @@ export default function ExecutionStatusIndicator({
   status: WorkflowExecutionStatus;
 }) {
   return (
-    <div className={cn("w-2 h-2 rounded-full", indicatroColors[status])} />
+    <div className={cn("w-2 h-2 rounded-full", indicatorColors[status])} />
   );
+}
+
+const labelColors: Record<WorkflowExecutionStatus, string> = {
+  PENDING: "text-slate-400 animate-pulse",
+  RUNNING: "text-yellow-400 animate-pulse",
+  FAILED: "text-red-400",
+  COMPLETED: "text-emerald-600",
+};
+
+export function ExecutionStatusLabel({
+  status,
+}: {
+  status: WorkflowExecutionStatus;
+}) {
+  return <span className={cn("lowercase", labelColors[status])}>{status}</span>;
 }
