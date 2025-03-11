f3d163f12d0e38b83af89c4fe050269c55a79fb8
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-11 21:41:08 +0530

    feat: add NavigateUrl task and executor for navigating to a specified URL

---
+++ lib/workflow/task/NavigateUrl.tsx
@@ -0,0 +1,31 @@
+import { TaskParamType, TaskType } from "@/types/task.types";
+import { WorkflowTask } from "@/types/workflow.types";
+import { Link, LucideProps, MousePointerClick, TextIcon } from "lucide-react";
+
+export const NavigateUrlTask = {
+  type: TaskType.NAVIGATE_URL,
+  label: "Navigate Url",
+  icon: (props: LucideProps) => (
+    <Link className="stroke-primary" {...props} />
+  ),
+  isEntryPoint: false,
+  credits: 2,
+  inputs: [
+    {
+      name: "Web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+      required: true,
+    },
+    {
+      name: "URL",
+      type: TaskParamType.STRING,
+      required: true,
+    },
+  ] as const,
+  outputs: [
+    {
+      name: "Web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+    },
+  ] as const,
+} satisfies WorkflowTask;
