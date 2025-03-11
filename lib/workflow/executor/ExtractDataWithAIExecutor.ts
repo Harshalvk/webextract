e4d207934106660b51687e5434aa993562e7047e
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-11 16:38:27 +0530

    feat: integrate ollama for AI-driven data extraction in ExtractDataWithAIExecutor

--- lib/workflow/executor/ExtractDataWithAIExecutor.ts
+++ lib/workflow/executor/ExtractDataWithAIExecutor.ts
@@ -2,6 +2,7 @@ import { ExecutionEnvironment } from "@/types/executor.types";
 import { ExtractDataWithAITask } from "../task/ExtractDataWithAI";
 import prisma from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
+import ollama from "ollama";
 
 export async function ExtractDataWithAIExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAITask>
@@ -37,13 +38,39 @@ export async function ExtractDataWithAIExecutor(
       return false;
     }
 
-    const mockExtractedData = {
-      usernameSelector: "#username",
-      passwordSelector: "#password",
-      loginSelector: "body > div.container > form > input.btn.btn-primary",
-    };
+    const response = await ollama.chat({
+      model: "llama3.2:1b",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid JSON array without any surrounding text.",
+        },
+        {
+          role: "user",
+          content: content,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      stream: false,
+      format: "json",
+    });
 
-    environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
+    environment.log.info(
+      `Prompt evaluation count: ${response.prompt_eval_count}`
+    );
+    environment.log.info(`Evaluation count: ${response.eval_count}`);
+
+    const result = response.message.content;
+    if (!result) {
+      environment.log.error("empty response from AI");
+      return false;
+    }
+
+    environment.setOutput("Extracted data", result);
 
     return true;
   } catch (error: any) {
