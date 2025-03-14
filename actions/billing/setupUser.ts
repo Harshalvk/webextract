50dd1421b34e196d0eb000602be5a84b180d8182
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-14 19:08:27 +0530

    feat: add user setup functionality and login page component

---
+++ actions/billing/setupUser.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
+
+export async function SetupUser() {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    throw new Error("Unauthenticated");
+  }
+
+  if (!user?.id) {
+    throw new Error("Invalid user ID. Please log in again.");
+  }
+
+  const balance = await prisma.userBalance.findUnique({
+    where: { userId: user.id },
+  });
+
+  if (!balance) {
+    // free 100 credits
+    await prisma.userBalance.create({
+      data: {
+        userId: user.id,
+        credits: 100,
+      },
+    });
+  }
+
+  redirect("/");
+}
---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import SignIn from "@/components/SignIn";
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900 p-4">
+      <SignIn />
+    </div>
+  );
+}
