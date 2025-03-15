2e64baa3563d7d1cebbc17c5a29acc1c62fa68a2
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-15 23:50:47 +0530

    feat: add PurchaseCredits function for processing credit purchases via Stripe

---
+++ actions/billing/purchaseCredits.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { auth } from "@/auth";
+import { getAppUrl } from "@/lib/helper/appUrl";
+import { stripe } from "@/lib/stripe/stripe";
+import { getCreditsPack, PackId } from "@/types/billing.types";
+import { redirect } from "next/navigation";
+
+export async function PurchaseCredits(packId: PackId) {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    throw new Error("Unauthenticated");
+  }
+
+  const userId = user.id;
+
+  if (!userId) {
+    throw new Error("Invalid user ID. Please log in again.");
+  }
+
+  const selectedPack = getCreditsPack(packId);
+  if (!selectedPack) {
+    throw new Error("invalid pack");
+  }
+  const priceId = selectedPack?.priceId;
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    mode: "payment",
+    invoice_creation: {
+      enabled: true,
+    },
+    success_url: getAppUrl("billing"),
+    cancel_url: getAppUrl("billing"),
+    metadata: {
+      userId,
+      packId,
+    },
+    line_items: [
+      {
+        quantity: 1,
+        price: selectedPack.priceId,
+      },
+    ],
+  });
+
+  if (!stripeSession.url) {
+    throw new Error("cannot create stripe session");
+  }
+
+  redirect(stripeSession.url);
+}
