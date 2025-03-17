c497ae4db4656ff684ad5f7268b2e763e7238810
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-17 11:53:45 +0530

    feat: add Spotlight component with animation to enhance landing page visuals

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -95,11 +95,22 @@ export default {
             transform: "translate3d(1px, 0, 0)",
           },
         },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         shake: "shake 0.82s cubic-bezier(.36,.07,.19,.97) both",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
   },
