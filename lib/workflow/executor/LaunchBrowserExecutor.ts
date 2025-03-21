819b35af2c0bfd0722c0c161541063df0cb761a1
Harshalvk <harshalvkhobragade@gmail.com>
2025-03-21 16:56:08 +0530

    fix: make puppeteer headless

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -9,7 +9,7 @@ export async function LaunchBrowserExecutor(
   try {
     const websiteUrl = environment.getInput("Website Url");
     const browser = await puppeteer.launch({
-      headless: false, // for testing
+      headless: true, // make it false when you want to see how puppeteer works visually
     });
     environment.setBrowser(browser);
     environment.log.info("Browser started successfully");
